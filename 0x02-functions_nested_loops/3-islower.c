5f07682b3d7732257f9b8bf31baeb9c71097866e
Taha <you@example.com>
2022-10-18 15:29:59 -0700

    Fix: bettey style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,7 +8,9 @@
 
 /**
  * _islower - check if the given char is in lowercase
+ * @c: The character to be checked.
  *
+ * Return: 1 if character is lowercase, 0 otherwise.
  */
 int _islower(char c)
 {
