ff7dead0b7727a2e177daa99d5efe4e5c31545ad
Taha <you@example.com>
2022-10-18 14:42:29 -0700

    Fix: some typoses

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,12 @@
 
 #include "main.h"
 
- /**
-  *  main - Prints "_putchar" followed by a new line.
-  * 
-  *  Return: Always 0.
-  *     
-  */
- int main(void)
+/**
+ * main - Prints "_putchar" followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
