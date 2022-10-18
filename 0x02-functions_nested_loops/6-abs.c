7d68029ef86f6ed4d802929baf2ad84cdc46bd50
Taha <you@example.com>
2022-10-18 16:00:48 -0700

    fetures : a function that computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -10,12 +10,12 @@
  * absolute - computes the absolute
  * @n: The number of which the absolute value will be computed.
  *
- * Return: the absolute of un integer */
+ * Return: the absolute of un integer 
+ */
 int _abs(int n)
 {
 
 	if (n < 0)
 		n *= -1;
-	
 	return (n);
 }
