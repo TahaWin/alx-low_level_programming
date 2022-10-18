59b5af3928f05e825f8f48e4f8dbe5c23efc2c5d
Taha <you@example.com>
2022-10-18 16:17:29 -0700

    fetures : a function that prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,16 @@
 /*
- *  * File: 7-print_last_digit.c
- *   * Auth: Brennan D Baraban
- *    */
+ * File: 7-print_last_digit.c
+ * Auth: Brennan D Baraban
+ */
 
 #include "main.h"
 
 /**
- *  * print_last_digit - Prints the last digit of a number.
- *   * @n: The number in question.
- *    *
- *     * Return: Value of the last digit.
- *      */
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
+ */
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
