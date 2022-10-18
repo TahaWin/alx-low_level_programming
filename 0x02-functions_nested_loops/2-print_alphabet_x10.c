2b738df71a65564d4c46778a321ebaa1a921b45b
Taha <you@example.com>
2022-10-18 15:06:43 -0700

    Fix: betty style issues

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,22 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet in lowercase 10 times, followed by a new line
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times, 
+ * followed by a new line
  *
  */
 
 void print_alphabet_x10(void)
 {
 	int i;
-
 	char c;
 	
 	for (i = 0; i < 10; i++)
+	{
 		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
+		_putchar('\n');
+	}
 
 	_putchar('\n');
 }
