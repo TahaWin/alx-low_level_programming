92fec8a52b9d7ab5ea56f49521faf7d8870367a1
Taha <you@example.com>
2022-10-18 17:01:09 -0700

    Feature: #8 solution

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
- * void jack_bauer(void) - prints minutes
+ * jack_bauer - prints minutes
  *
  */
 void jack_bauer(void)
@@ -14,17 +14,15 @@ void jack_bauer(void)
 	int i;
 	int j;
 
-	for(i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for(j = 0; j < 60; j++)
+		for (j = 0; j < 60; j++)
 		{
-			if (i < 10)
-				_putchar('0');
-			_putchar(i);
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			if (j < 10)
-				_putchar('0');
-			_putchar(j);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 
