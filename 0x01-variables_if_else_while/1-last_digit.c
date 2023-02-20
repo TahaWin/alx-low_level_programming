4856f6a0ca880979bd94ced5a8e2f7c74f5054bc
TahaWin <tahawen@gmail.com>
2023-02-20 14:50:44 -0800

    again stubit mistake

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n",
 			n, n % 10);
 	}
-	if else
+	else if
 	{
 		printf("Last digit of %d is %d and is 0\n",
 			n, n % 10);
