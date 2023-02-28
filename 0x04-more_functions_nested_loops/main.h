bd5e66bd815ccffcf4247819c0347983bc73084a
TahaWin <tahawen@gmail.com>
2023-02-28 15:59:25 -0800

    sssssssssssssss

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,19 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
- * File: main.h
- * Auth: Taha Ahmed Khater
- * Desc: Header file containing declarations for all functions
- * used in the 0x04-more_functions_nested_loops directory.
- */
-
-
-int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
