185b6ee6cca747fc57b85b5e181d30b1bace42cd
TahaWin <tahawen@gmail.com>
2023-04-10 16:05:37 -0700

    fixing the main erorr

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,15 @@
-#include "main.h"
-/**
- * get_endianness - return the endianness of the machine
- *
- * Return: 0 for big endian, 1 for little endian
- */
-int get_endianness(void)
-{
-	int n = 1;
+#ifndef MAIN_H
+#define MAIN_H
 
-	return (*((char *) &n) + '0');
-}
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
